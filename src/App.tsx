7b9e3b8f10193fc8c3b3441306708896b63f15ea
eminebozdag <fyrs00@hotmail.com>
2022-12-26 07:52:40 +0100

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">hey</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
