a13428a1c4a4e1276e54ebd6cea404d327f65957
eminebozdag <fyrs00@hotmail.com>
2022-12-27 22:41:58 +0100

    Add styles and lib

---
+++ src/lib/meta.ts
@@ -0,0 +1,17 @@
+const MetaConfig = {
+  title: "emine bozdag",
+  shortDescription: "Software Engineer, Art-Lover and Enthusiastic",
+  description:
+    "Software Engineer, Art-Lover and Enthusiastic. I write blog posts about engineering and other topics I am passionate about. Also creating some open source projects to share my knowledge with the community.",
+  url: "",
+  social: {
+    email: "fyrs00@hotmail.com",
+    instagram: "https://instagram.com/fbozd4g",
+    twitter: "https://twitter.com/eminebozd4g",
+    medium: "https://medium.com/@eminebozdag",
+    github: "https://github.com/eminebozdag",
+    linkedin: "https://linkedin.com/in/eminebozdag",
+  },
+};
+
+export default MetaConfig;
